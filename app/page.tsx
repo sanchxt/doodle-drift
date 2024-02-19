ac3b3e59701f46c2b0942bc6369d8aabe35d15f1
sanchxt <tpbeast007@gmail.com>
2024-02-19 22:04:09 +0530

    fixed convex & clerk

--- app/page.tsx
+++ app/page.tsx
@@ -1,5 +1,12 @@
-import { Button } from "@/components/ui/button";
+import { UserButton } from "@clerk/nextjs";
 
 export default function Home() {
-  return <Button>Click Me</Button>;
+  return (
+    <div className="flex flex-col gap-y-4">
+      <div>This is a screen for authenticated users only</div>
+      <div>
+        <UserButton />
+      </div>
+    </div>
+  );
 }
---
+++ convex/auth.config.js
@@ -0,0 +1,8 @@
+export default {
+  providers: [
+    {
+      domain: "https://modern-stallion-18.clerk.accounts.dev",
+      applicationID: "convex",
+    },
+  ],
+};
