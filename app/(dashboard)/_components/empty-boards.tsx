8d04bfc01f5d4435cf1b21242eda52d77046c396
sanchxt <tpbeast007@gmail.com>
2024-02-25 23:11:43 +0530

    added empty states

---
+++ app/(dashboard)/_components/empty-boards.tsx
@@ -0,0 +1,44 @@
+"use client";
+
+import Image from "next/image";
+import { toast } from "sonner";
+
+import { api } from "@/convex/_generated/api";
+import { Button } from "@/components/ui/button";
+import { useOrganization } from "@clerk/nextjs";
+import { useApiMutation } from "@/hooks/use-api-mutation";
+
+export const EmptyBoards = () => {
+  const { organization } = useOrganization();
+  const { mutate, pending } = useApiMutation(api.board.create);
+
+  const onClick = () => {
+    if (!organization) return;
+
+    mutate({
+      orgId: organization.id,
+      title: "Untitled",
+    })
+      .then((id) => {
+        toast.success("Board created successfully");
+        // TODO: redirect to the created board
+      })
+      .catch(() => toast.error("Failed to create the board"));
+  };
+
+  return (
+    <div className="h-full flex flex-col items-center justify-center">
+      <Image src="/note.svg" alt="Empty Boards Icon" height={110} width={110} />
+      <h2 className="text-2xl font-semibold mt-6">Create your first board</h2>
+      <p className="text-muted-foreground text-sm mt-2">
+        Start by creating a board for your organization
+      </p>
+
+      <div className="mt-6">
+        <Button size="lg" onClick={onClick} disabled={pending}>
+          Create board
+        </Button>
+      </div>
+    </div>
+  );
+};
