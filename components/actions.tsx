18abc7f9e8bf154cbb9fd0c0f2b658cb163a6b6f
sanchxt <tpbeast007@gmail.com>
2024-03-08 14:08:02 +0530

    testing commit

--- components/actions.tsx
+++ components/actions.tsx
@@ -80,7 +80,7 @@ export const Actions = ({
             variant="ghost"
             className="p-3 cursor-pointer text-sm w-full justify-start font-normal"
           >
-            <Trash2 className="h-4 w-4 mr-2" /> Delete
+            <Trash2 className="h-4 w-4 mr-2" /> Delete board
           </Button>
         </ConfirmModal>
       </DropdownMenuContent>
