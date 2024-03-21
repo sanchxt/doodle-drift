9ccc9df7e9e1d381b7030b063a62d1206e853358
sanchxt <tpbeast007@gmail.com>
2024-03-21 15:13:44 +0530

    fix favorite schema bug

--- convex/schema.ts
+++ convex/schema.ts
@@ -21,6 +21,6 @@ export default defineSchema({
   })
     .index("by_board", ["boardId"])
     .index("by_user_org", ["userId", "orgId"])
-    .index("by_board_board", ["userId", "boardId"])
+    .index("by_user_board", ["userId", "boardId"])
     .index("by_user_board_org", ["userId", "boardId", "orgId"]),
 });
