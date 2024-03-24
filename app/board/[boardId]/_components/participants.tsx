46a2353dffc9fa3f22734e31f5b4f046cf441958
sanchxt <tpbeast007@gmail.com>
2024-03-24 20:10:10 +0530

    add use client

--- app/board/[boardId]/_components/participants.tsx
+++ app/board/[boardId]/_components/participants.tsx
@@ -1,3 +1,5 @@
+"use client";
+
 export const Participants = () => {
   return (
     <div className="absolute h-12 top-2 right-2 bg-white rounded-md p-3 flex items-center shadow-md">
@@ -6,7 +8,7 @@ export const Participants = () => {
   );
 };
 
-Participants.Skeleton = function ParticipantsSkeleton() {
+export const ParticipantsSkeleton = () => {
   return (
     <div className="absolute h-12 top-2 right-2 bg-white rounded-md p-3 flex items-center shadow-md w-[100px]" />
   );
