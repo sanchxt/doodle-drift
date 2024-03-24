9cf0705077740de86156a3f591f32bfaf15c4f6e
sanchxt <tpbeast007@gmail.com>
2024-03-24 20:15:57 +0530

    add interface to accept board-id prop

--- app/board/[boardId]/_components/info.tsx
+++ app/board/[boardId]/_components/info.tsx
@@ -1,6 +1,10 @@
 "use client";
 
-export const Info = () => {
+interface InfoProps {
+  boardId: string;
+}
+
+export const Info = ({ boardId }: InfoProps) => {
   return (
     <div className="absolute top-2 left-2 bg-white rounded-md px-1.5 h-12 flex items-center shadow-md">
       todo: board info here
