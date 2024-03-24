fe157313f325d163577e69b6f396a83e9fe1f2b5
sanchxt <tpbeast007@gmail.com>
2024-03-24 20:47:04 +0530

    fix import to match updated file name

--- providers/modal-provider.tsx
+++ providers/modal-provider.tsx
@@ -2,7 +2,7 @@
 
 import { useEffect, useState } from "react";
 
-import { RenameModal } from "@/components/modals/rename-model";
+import { RenameModal } from "@/components/modals/rename-modal";
 
 export const ModalProvider = () => {
   const [isMounted, setIsMounted] = useState(false);
